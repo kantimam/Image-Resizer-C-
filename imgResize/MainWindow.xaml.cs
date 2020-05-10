cd408913f11f65f76beca83c549df9c982ec75dc
Nyannazz <kantemir.imam@gmail.com>
2020-05-10 22:17:22 +0200

    added default options

--- imgResize/MainWindow.xaml.cs
+++ imgResize/MainWindow.xaml.cs
@@ -14,6 +14,16 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.IO;
 using PhotoSauce.MagicScaler;
+using System.Text.RegularExpressions;
+using System.ComponentModel;
+
+public class Option
+{
+    public string Name { get; set; }
+    public int Width { get; set; }
+    public int Height { get; set; }
+    public string Mode { get; set; }
+}
 
 namespace imgResize
 {
@@ -27,17 +37,100 @@ namespace imgResize
         {
             InitializeComponent();
 
+            createFolder();
+
+            fillOptions();
+            
+        }
+
+        // default Settings
+        private ProcessImageSettings defaultSetting = new ProcessImageSettings()
+        {
+            Width = 480,
+            Height = 480,
+            ResizeMode = CropScaleMode.Max,
+            SaveFormat = FileFormat.Jpeg,
+            JpegQuality = 75,
+            JpegSubsampleMode = ChromaSubsampleMode.Subsample420
+        };
+
+        private ProcessImageSettings settings = new ProcessImageSettings()
+        {
+            Width = 480,
+            Height = 480,
+            ResizeMode = CropScaleMode.Max,
+            SaveFormat = FileFormat.Jpeg,
+            JpegQuality = 75,
+            JpegSubsampleMode = ChromaSubsampleMode.Subsample420
+        };
+
+        private List<Option> settingsList = new List<Option>()
+        {
+            new Option()
+            {
+                Name="Full HD",
+                Width=1920,
+                Height=1080,
+                Mode="contain"
+            },
+            new Option()
+            {
+                Name="HD",
+                Width=1280,
+                Height=720,
+                Mode="contain"
+            },
+            new Option()
+            {
+                Name="Mobile",
+                Width=480,
+                Height=0,
+                Mode="contain"
+            }
+        }; 
+
+        private void createFolder()
+        {
             // create the output directory if it does not exist
             var picPath = @"C:\Users\Schwartza\Pictures";
             this.outputDirectory = System.IO.Path.Combine(picPath, "resizedPictures");
 
-            if (!Directory.Exists(this.outputDirectory)){
+            if (!Directory.Exists(this.outputDirectory))
+            {
                 Directory.CreateDirectory(this.outputDirectory);
             }
         }
 
+        private void fillOptions()
+        {
+            this.sizeOptionsList.ItemsSource = settingsList;
+        }
+
         
         private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var fileArr=getImageFiles();
+
+            if (fileArr.Count()>0)
+            {
+                this.openStatus.Text = "selected "+fileArr.Count()+" images";
+
+
+                // get dimensions from input field
+                int width = Int32.Parse(this.widthTextBox.Text);
+                resizeImagesParallel(fileArr, width);
+                
+            }
+            else
+            {
+                //
+                this.openStatus.Text = "Could not open file";
+
+            }
+        }
+
+
+        private void Button_Click_Old(object sender, RoutedEventArgs e)
         {
             this.openStatus.Text = "clicked";
 
@@ -54,7 +147,7 @@ namespace imgResize
                 return;
             }
 
-            if (result!=false)
+            if (result != false)
             {
                 const int size = 200;
                 const int quality = 75;
@@ -75,7 +168,7 @@ namespace imgResize
                     this.openStatus.Text = "Picked photo" + fileName;
                     resizeImage(fileName, 500);
                 }
-                
+
             }
             else
             {
@@ -84,6 +177,7 @@ namespace imgResize
 
             }
         }
+
 
         private List<string> getImageFiles()
         {
@@ -116,19 +210,10 @@ namespace imgResize
 
         private void resizeImage(string input, int size)
         {
-            var settings = new ProcessImageSettings()
-            {
-                Width = size,
-                Height = size,
-                ResizeMode = CropScaleMode.Max,
-                SaveFormat = FileFormat.Jpeg,
-                JpegQuality = 75,
-                JpegSubsampleMode = ChromaSubsampleMode.Subsample420
-            };
 
             using (var output = new FileStream(OutputPath(input, outputDirectory), FileMode.Create))
             {
-                MagicImageProcessor.ProcessImage(input, output, settings);
+                MagicImageProcessor.ProcessImage(input, output, this.settings);
             }
 
         }
@@ -144,5 +229,37 @@ namespace imgResize
                 + System.IO.Path.GetExtension(inputPath)
            );
         }
-    }
+
+        private void heightTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void AllowNumbers(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+
+        private void OptionSelected(object sender, EventArgs e)
+        {
+            Option options=(Option)this.sizeOptionsList.SelectedItem;
+            this.openStatus.Text = options.Name;
+            this.widthTextBox.Text = options.Width.ToString();
+            this.heightTextBox.Text = options.Height.ToString();
+            SelectOption(options);
+            return;
+        }
+
+        private void SelectOption(Option option)
+        {
+            this.settings.Width = option.Width;
+            this.settings.Height = option.Height;
+            
+            this.settings.ResizeMode =
+                option.Mode=="crop" ? CropScaleMode.Crop : CropScaleMode.Max;
+            
+            
+        }
+}
 }
